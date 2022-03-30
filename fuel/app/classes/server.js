2adda5c240d5f95cd6dc143632c24b9a70c01262
Ahsen <ahsen.waqar@gmail.com>
2022-03-30 13:33:29 -0600

    Add files via upload

---
+++ fuel/app/classes/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(parser.json);
+app.use('/', require('./controllers/renderTables'));
+const PORT = process.env.PORT || 4444;
+app.listen(PORT,  console.log("Server is running at: " + PORT));
+
+app.use(express.urlencoded({extended: false}));
+
+
+
+
+
