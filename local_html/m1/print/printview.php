084c4d0380534cdeeee7ba6e1b5282944a16c4ee
Tor Larson <72530041+torlarso@users.noreply.github.com>
2022-03-30 15:59:46 -0600

    Create printview.php

---
+++ local_html/m1/print/printview.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset= "utf-8">
+        <title>Color Coordinate Generation</title>
+        <link rel="stylesheet" href="printstyle.css" />
+    </head>
+
+    <body>
+        <header>
+            <h1><img src="apfinal.jpg" alt="Company Logo" width="50" height="60"> Company 26</h1>
+        </header>
+            <!-- Insert generated tables here -->
+        </main>
+        <main>
+    <footer>
+    </footer>
+    </body>
